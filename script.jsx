3904ba506575adfb8f8e4b742268d692511043ac
irockmysock <benny_chin89@hotmail.com>
2019-09-01 01:15:51 +0800

    added mark completed function and completed list div

--- script.jsx
+++ script.jsx
@@ -5,13 +5,15 @@ class Main extends React.Component {
     this.state = {
       list : [],
       word: "",
-      inputErrorMsg: ""
+      inputErrorMsg: "",
+      completedList: ["eat bak ku teh"]
     };
 
     this.setWord = this.setWord.bind(this);
     this.addItem = this.addItem.bind(this);
     this.deleteItem = this.deleteItem.bind(this);
     this.editItem = this.editItem.bind(this);
+    this.markComplete = this.markComplete.bind(this);
   }
 
   addItem(){
@@ -54,6 +56,17 @@ class Main extends React.Component {
     this.setState({inputErrorMsg: ""})
   }
 
+  markComplete(e) {
+    let updatedList = this.state.list;
+    let completedItem = updatedList.splice(e.target.value, 1);
+    console.log(completedItem);
+    this.setState({list: updatedList})
+
+    let completedList = this.state.completedList;
+    completedList.push(completedItem[0]);
+    this.setState({completedList: completedList});
+  }
+
 
   render() {
       console.log("rendering MAIN APP");
@@ -74,17 +87,11 @@ class Main extends React.Component {
                   LIST={this.state.list} 
                   EDITItem={this.editItem} 
                   DELETEItem={this.deleteItem}
+                  markComplete={this.markComplete}
               />
               </div>
             </div>
-            <div className="col-md-6">
-              <div className="todolist">
-              <h1>Completed Tasks</h1>
-                  <ul id="done-items" className="list-unstyled">
-                      <li>Some item <button className="remove-item btn btn-default btn-xs pull-right"><span class="glyphicon glyphicon-remove"></span></button></li>   
-                  </ul>
-              </div>
-            </div>
+            <CompletedList completedList={this.state.completedList}/>
           </div>
         </div>
       );
@@ -104,7 +111,7 @@ class Form extends React.Component {
               value={this.props.word}
         /> 
         <button 
-              class="btn btn-success"
+              className="btn btn-success"
               onClick={ this.props.addItem }> Add Item </button>
         <p className="err-msg">{this.props.inputErrorMsg}</p>
       </React.Fragment>
@@ -120,7 +127,8 @@ class TableCom extends React.Component {
               <ListItems 
                       key={id}
                       EDIT={this.props.EDITItem} 
-                      DELETE={this.props.DELETEItem} 
+                      DELETE={this.props.DELETEItem}
+                      markComplete={this.props.markComplete} 
                       item={item} 
                       id={id}
               />
@@ -134,6 +142,7 @@ class TableCom extends React.Component {
                       <th>ID</th>
                       <th>ToDo Item</th>
                       <th>Date Added</th>
+                      <th>Action</th>
                       <th>Action</th>
                       <th>Action</th>
                   </tr>
@@ -213,8 +222,31 @@ class ListItems extends React.Component {
               <td>{moment().format("MMM Do YY HH:mm")}</td>
               <td><button onClick={this.startEdit}>Edit</button></td>
               <td><button value={this.props.id} onClick={this.props.DELETE}>Delete</button></td>
+              <td><button value={this.props.id} onClick={this.props.markComplete}>Mark Complete</button></td>
           </tr>
       )
+  }
+}
+
+
+class CompletedList extends React.Component {
+  render() {
+    let listItems = this.props.completedList.map( (item, id) => {
+      return (
+        <li key={id}>{item} <button className="remove-item btn btn-default btn-xs pull-right"><span className="glyphicon glyphicon-remove"></span></button></li>
+      )
+    });  
+
+    return (
+      <div className="col-md-6">
+        <div className="todolist">
+          <h1>Completed Tasks</h1>
+          <ul id="done-items" className="list-unstyled">
+            {listItems}   
+          </ul>
+        </div>
+      </div>
+    );
   }
 }
 
